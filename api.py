4f048d848cc674087248589a2070b3074b015924
Exenifix <yeligar@ya.ru>
2022-03-14 15:50:13 +0300

    Added logging

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from os import getenv
 
 from aiohttp import ClientSession, web
 from dotenv import load_dotenv
+from exencolorlogs import Logger
 
 load_dotenv()
 APIKEY = getenv("APIKEY")
@@ -9,14 +10,19 @@ PORT = getenv("PORT")
 
 app = web.Application()
 route = web.RouteTableDef()
+log = Logger()
 
 
 @route.get("/weather")
 async def get_weather(req: web.Request):
+    log.info("Received weather request from %s", req.remote)
     try:
         city = req.rel_url.query["city"]
     except KeyError:
-        return web.Response(status=400)
+        log.warning("Request is mising city param")
+        return web.json_response(
+            {"error": '400 Invalid Form Body: missing "city" param'}, status=400
+        )
 
     async with ClientSession() as session:
         r = await session.get(
@@ -26,6 +32,7 @@ async def get_weather(req: web.Request):
         data = await r.json()
 
     if r.status != 200:
+        log.error("Internal server error")
         print(r.status)
         print(data)
         return web.Response(status=500)
@@ -34,4 +41,5 @@ async def get_weather(req: web.Request):
 
 
 app.add_routes(route)
+log.info("Running on port %s", PORT)
 web.run_app(app, port=PORT)
